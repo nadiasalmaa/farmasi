dab153f46ad860974aade7d245c12a1ce4902ef3
Nadia Salma <ndsalmahftr733@gmail.com>
2025-12-18 21:27:06 +0700

    chore: prepare deployment (start scripts, env PORT, Procfile, vercel.json)

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { Pool } = require('pg');
 const { v4: uuidv4 } = require('uuid'); 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
